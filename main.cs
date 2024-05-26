0b760fc654e5545db3b0600438af9f03a47083b3
Heitor Girelli <heitor.girelli@hotmail.com>
2024-05-26 15:31:52 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,11 @@
+using static System.Console;
+// This program displays a song lyric 
+class Program {
+  public static void Main () {
+    WriteLine ("Now, this is a story all about how"); // Stanza 1
+    WriteLine ("My life got flipped-turned upside down"); // Stanza 2
+    WriteLine ("And I'd like to take a minute"); // Stanza 3
+    WriteLine ("Just sit right there"); // Stanza 4
+    WriteLine ("I'll tell you how I became the prince of a town called Bel-Air"); // Stanza 5
+  } // End of class
+} // End of Program
